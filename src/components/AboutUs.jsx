bd0554890797a227f4029605e1842e528e972e55
Nikhil Pandey <pandeynikhilone@gmail.com>
2025-08-04 09:56:27 +0530

    +1 AboutUs| Responsive | 04-08-2025

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div>
+      <div className="flex flex-col text-center mx-auto p-4 bg-[#234540] mt-5 rounded-t-[40px]">
+        <div className="text-[32px] lg:text-[38px] font-bold mt-4">
+          About
+          <span className="Comic text-[#fdb235]"> us</span>
+        </div>
+        <div className="">
+          <div className=" text-justify p-6 text-[18px] md:text-[21px] text-zinc-400 Lato">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis
+            quidem ducimus laborum quae nostrum expedita sint ipsa amet est!
+            Magnam necessitatibus aspernatur quibusdam iure vero. Lorem ipsum
+            dolor sit, amet consectetur adipisicing elit. Veritatis quidem
+            ducimus laborum quae nostrum expedita sint ipsa amet est! Magnam
+            necessitatibus aspernatur quibusdam iure vero.
+          </div>
+
+          <div className="flex justify-end">
+            <img className="w-[150px]" src="/Logo-1.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs
--- src/components/NGO_Home.jsx
+++ src/components/NGO_Home.jsx
@@ -1,10 +1,49 @@
-import Button from './Button.jsx';
-import Navigation from './nav/Navigation.jsx'
+// NGO_Home Component
+import AboutUs from "./AboutUs.jsx";
+import Button from "./Button.jsx";
+import Navigation from "./nav/Navigation.jsx";
+
+const CardSection = () => {
+  const data = [
+    "Promote your business, support humanity with our products",
+    "Mark your calendar: Exciting programs on 15 September at",
+    "Celebrating our latest achievement: 50K happy members",
+  ];
+
+  const img = [
+    "/icons/program-1.png",
+    "/icons/program 2.png",
+    "/icons/program 3.png",
+  ];
+
+  // Combine text and icon into one array
+  const cards = data.map((text, i) => ({
+    text,
+    icon: img[i],
+  }));
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mt-6">
+      {cards.map((card, i) => (
+        <div
+          key={i}
+          className="bg-zinc-200 p-7 flex flex-col gap-5 rounded-2xl w-full sm:w-[300px] md:w-[342px] h-[226px] hover:translate-y-1.5 duration-200"
+        >
+          <img className="w-[56px]" src={card.icon} alt="card icon" />
+          <div>{card.text}</div>
+          <span className="text-[#fdb235] w-fit font-bold hover:cursor-pointer">
+            GET HELP
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function NGO_Home() {
   return (
     <>
-      <div className="flex gap-[85px] flex-col bg-[#234540]">
+      <div className="flex gap-[45px] flex-col bg-[#234540]">
         <Navigation />
         <main className="flex flex-col mx-auto justify-center md:w-[840px] w-full Lato gap-4">
           <div className="text-[32px] md:text-[50px] lg:text-[53px] mx-auto text-white font-bold text-center ">
@@ -20,7 +59,8 @@ function NGO_Home() {
           </div>
           <Button />
         </main>
-        <section className="mx-auto">
+
+        <section className="mx-auto mb-5">
           <div className="gap-10 hidden md:flex ">
             <img src="/image copy.png" alt="small baby" />
             <img src="/image copy 2.png" alt="small baby" />
@@ -35,14 +75,12 @@ function NGO_Home() {
           Our upcoming
           <span className="Comic text-[#fdb235]"> Programs</span>
         </div>
-          <div className="rounded-[4px] bg-zinc-500">
-            <img src="" alt="" />
-            <div>Promote your business, support humanity with our products</div>
-            <span>GET HELP</span>
-          </div>
+
+        <CardSection />
       </section>
+      <AboutUs/>
     </>
   );
 }
 
-export default NGO_Home
+export default NGO_Home;
